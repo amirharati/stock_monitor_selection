6c4b296cdc79d5d5f9c3d0e211cd0ef55bf16fc7
amir <amir.harati@gmail.com>
2019-12-14 22:58:38 -0500

    add state machine for parsing

--- process_data.py
+++ process_data.py
@@ -17,9 +17,14 @@ import yaml
 import logging
 import numpy as np 
 import pandas as pd
+import re
 
 class ProcessData:
     def __init__(self):
+        # define patterns
+        pat_fin = "Financials"
+        self.pat_fin = re.compile(pat_fin)
+
         self._fyahoo_hist = {}
 
     def _read_ticker_list(self, ticker_list):
@@ -57,7 +62,90 @@ class ProcessData:
         self._fyahoo_hist[ticker_id] = pd.read_csv(ticker_file)
 
     def _load_ticker_morningstar_key(self, ticker_id, ticker_file):
-        pass
+        FIN_DATA = {}
+
+        # define a simple finite state machine to parse the file
+        state = "INIT"
+        lines = [line.strip() for line in open(ticker_file)]
+        #ASSUMING orders are always the same. Otherwise need to do more pattern matchings
+        for line in lines:
+            m = self.pat_fin.match(line)
+            if m is not None:
+                state = "FIN" # 
+                continue
+            elif state == "FIN":
+                state = "FIN-YEAR"
+                fin_years = line.split(",")[1:]
+                FIN_DATA["years"] = fin_years
+                continue
+            elif state == "FIN-YEAR":
+                state = "FIN-REV"
+                parts = line.splir(",")
+                curr = " ".join(parts[0].split()[1:])
+                FIN_DATA["REV_CUURENCY"] = curr
+                FIN_DATA["REV"] = parts[1:]
+                continue
+            elif state == "FIN-REV":
+                state = "FIN-GROSS"
+                pass # TODO: parse
+                continue
+            elif state == "FIN-GROSS":
+                state = "FIN-OPINC"
+                pass # TODO
+                continue
+            elif state == "FIN-OPINC":
+                state = "FIN-OPMARG"
+                pass
+                continue
+            elif state == "FIN-OPMARG"
+                state = "FIN-NETINC"
+                pass
+                continue
+            elif state == "FIN-NETINC":
+                state = "FIN-EPS"
+                pass
+                continue
+            elif state == "FIN-EPS":
+                state = "FIN-DIV"
+                pass
+                continue
+            elif state == "FIN-DIV":
+                state = "FIN-PAYOUT"
+                pass
+                continue
+            elif state == "FIN-PAYOUT"
+                state = "FIN-SHARE"
+                pass
+                continue
+            elif state == "FIN-SHARE":
+                state = "FIN-BPS"
+                pass
+                continue
+            elif state == "FIN-BPS":
+                state = "FIN-OPCASH"
+                pass
+                continue
+            elif state == "FIN-OPCASH":
+                state = "FIN-CAPSPEND"
+                pass
+                continue
+            elif state == "FIN-CAPSPEN":
+                state = "FIN-FREECASH"
+                pass
+                continue
+            elif state == "FIN-FREECASH":
+                state = "FIN-FRECASHSHARE" # free cash per share 
+                pass
+                continue
+            elif state == "FIN-FREECASHSHARE":
+                state = "FIN-WOCAP"
+                pass
+                continue
+            
+
+
+
+
 
     def _compute_yield(self):
         pass
